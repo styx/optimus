85a3800c1d47ad6cba4c05a36060191593214e3e
Mikhail S. Pobolovets <styx.mp@gmail.com>
2020-05-17 16:05:32 +0300

    Factorize

--- lib/optimus.ex
+++ lib/optimus.ex
@@ -8,27 +8,27 @@ defmodule Optimus do
 
   ## Examples
 
-      iex> Optimus.primes(1)
-      []
+  iex> Optimus.atkin(1)
+  []
 
-      iex> Optimus.primes(2)
-      [2]
+  iex> Optimus.atkin(2)
+  [2]
 
-      iex> Optimus.primes(3)
-      [2, 3]
+  iex> Optimus.atkin(3)
+  [2, 3]
 
-      iex> Optimus.primes(100)
-      [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-
+  iex> Optimus.atkin(100)
+  [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
   """
-  def primes(m) when m < 2, do: []
 
-  def primes(2), do: [2]
+  def atkin(m) when m < 2, do: []
 
-  def primes(3), do: [2, 3]
+  def atkin(2), do: [2]
 
-  def primes(m) do
+  def atkin(3), do: [2, 3]
+
+  def atkin(m) do
     limit = trunc(:math.sqrt(m)) + 1
     sieve = :array.new(m + 1, {:default, false})
 
@@ -106,5 +106,77 @@ defmodule Optimus do
   defp reset_quadratics(array, n, i) do
     :array.set(i, false, array)
     |> reset_quadratics(n, i * i)
+  end
+
+  @doc """
+  Factorize number
+
+  ## Examples
+
+  iex> Optimus.factorize(1)
+  %{}
+
+  iex> Optimus.factorize(2)
+  %{2 => 1}
+
+  iex> Optimus.factorize(4)
+  %{2 => 2}
+
+  iex> Optimus.factorize(8)
+  %{2 => 3}
+
+  iex> Optimus.factorize(10)
+  %{2 => 1, 5 => 1}
+
+  iex> Optimus.factorize(125)
+  %{5 => 3}
+  """
+
+  def factorize(n) do
+    primes =
+      n
+      |> div(2)
+      |> Kernel.+(1)
+      |> atkin
+
+    do_factorize(n, n, primes, %{})
+  end
+
+  def factorize(n, big_list_of_primes) do
+    limit =
+      n
+      |> div(2)
+      |> Kernel.+(1)
+
+    primes = Enum.take_while(big_list_of_primes, limit)
+    do_factorize(n, n, primes, %{})
+  end
+
+  defp do_factorize(1, _n, _primes, factors), do: factors
+
+  defp do_factorize(_current_n, n, [], %{}), do: %{n => 1}
+
+  defp do_factorize(current_n, _n, [], factors) do
+    {_, new_factors} =
+      Map.get_and_update(factors, current_n, fn power ->
+        {power, (power || 0) + 1}
+      end)
+
+    new_factors
+  end
+
+  defp do_factorize(current_n, n, [prime | rest_primes] = primes, factors) do
+    if Integer.mod(current_n, prime) == 0 do
+      {_, new_factors} =
+        Map.get_and_update(factors, prime, fn power ->
+          {power, (power || 0) + 1}
+        end)
+
+      current_n
+      |> div(prime)
+      |> do_factorize(n, primes, new_factors)
+    else
+      do_factorize(current_n, n, rest_primes, factors)
+    end
   end
 end
